50e4960d0065d17332511ec4d3259d9326779983
DimasCW <cdimas292@gmail.com>
2024-10-19 10:21:33 +0700

    Menambahkan "Login" dan "ini adalah halaman Login"

---
+++ app/src/main/java/com/example/pduabasic_compose/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.pduabasic_compose
+
+import android.graphics.Paint.Align
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pduabasic_compose.ui.theme.Pduabasic_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pduabasic_composeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicColumn(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier){
+    Text(
+        text = "Hello $name",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun BasicColumn(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 10.dp)
+    ){
+        Text("Login",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text("Ini adalah halaman login",
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic)
+
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text(
+            text = "Jetpack Compose",
+            fontSize = 50.sp,
+            fontStyle = FontStyle.Italic
+
+        )
+        Spacer(modifier = Modifier.padding(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.baju),
+            contentDescription = "",
+            modifier =Modifier.clip(CircleShape)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pduabasic_composeTheme {
+        Greeting("Android")
+    }
+}
